600341b5ad7fe8ee9debde5e041201628a87e065
RAtayev <44782839+RAtayev@users.noreply.github.com>
2019-05-16 23:47:16 +0300

    alfa

--- site/main.js
+++ site/main.js
@@ -1,9 +1,21 @@
+var main = document.getElementById("main_content");
+var statistic = document.getElementById("statistic_content");
+var autosArr = [{name: "Renault Koleos", price: "от 50000 BYN", imSrc: "images/koleos.jpg"}];
+
 function mainPage(){
-    document.getElementsByClassName("content")[0].innerText = "Главная страница";
+    document.getElementById("main").style.borderBottom = "6px solid orange";
+    document.getElementById("statistic").style.borderBottom = "none";
+    main.style.display = "";
+    statistic.style.display = "none";
+    console.log("Main");
 }
 
 function statisticPage(){
-    document.getElementsByClassName("content")[0].innerText = "Статистика";
+    document.getElementById("main").style.borderBottom = "none";
+    document.getElementById("statistic").style.borderBottom = "6px solid orange";
+    main.style.display = "none";
+    statistic.style.display = "";
+    console.log("Statistic");
 }
 
 function locationHashChanged(){
@@ -25,3 +37,37 @@ document.getElementById("statistic").addEventListener("click", event => {
 
 window.onload = onLoad;
 window.onhashchange = locationHashChanged;
+
+function addAutos(autosArray){
+    var autos = document.getElementById("autos");
+    for(let i = 0; i < autos.length; i++)
+    {
+        let auto = document.createElement('div');
+        auto.className = "auto";
+        autos.appendChild(auto);
+        let nameAuto = document.createElement('div');
+        nameAuto.className = "nameAuto";
+        nameAuto.innerHTML = autosArray[i].keys()[0];
+        let priceAuto = document.createElement('div');
+        priceAuto.className = "priceAuto";
+        priceAuto.innerHTML = autosArray[i].keys()[1];
+        let autoImageContainer = document.createElement('div');
+        autoImageContainer.className = "autoImageContainer";
+        let autoImage = document.createElement('img');
+        autoImage.id = "autoImage";
+        autoImage.src = autosArray[i].keys()[2];
+        autoImage.alt = autosArray[i].keys()[0];
+        let orderAuto = document.createElement('button');
+        orderAuto.className = "orderAuto";
+        orderAuto.id = "order" + autosArray[i].keys()[0];
+        orderAuto.innerHTML = "ЗАКАЗАТЬ";
+        let tempAuto = document.getElementsByClassName("auto");
+        tempAuto[tempAuto.length - 1].appendChild(nameAuto);
+        tempAuto[tempAuto.length - 1].appendChild(priceAuto);
+        tempAuto[tempAuto.length - 1].appendChild(autoImageContainer);
+        tempAuto[tempAuto.length - 1].childNodes[2].appendChild(autoImage);
+        tempAuto[tempAuto.length - 1].appendChild(orderAuto);
+    }
+}
+
+addAutos(autosArr);
